83ced8173f07feb643553bf12eb4c78fdecfa085
alla81y <alla.maan1399@gmail.com>
2025-02-19 04:16:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-clientId: "", // ايدي البوت
-    guildId: "", // ايدي السيرفر
-    token: "", // توكن البوت
-    CommandPermissions: "" // ايدي الرتبة الى تقدر تستخدم الامر
-}
+clientId: "1341144236640436334", // ايدي البوت
+    guildId: "1331648618179854417", // ايدي السيرفر
+    token: "MTM0MTE0NDIzNjY0MDQzNjMzNA.GAH9cp.YrsX_7wHRjrP1E_HLhZfXL-LA3q-dQyXwKd6rk", // توكن البوت
+    CommandPermissions: "1331649598095102043" // ايدي الرتبة الى تقدر تستخدم الامر
+};
